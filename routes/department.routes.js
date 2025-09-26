41f77e136581719478a2ebaa58d8b391881d6044
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-26 21:15:54 +0300

    fix: update db connection options

--- routes/department.routes.js
+++ routes/department.routes.js
@@ -11,8 +11,8 @@ const { authenticateToken } = require("../middleware/authenticate.middleware");
 const authorize = require("../middleware/authorize.middleware");
 
 app.post("/", authenticateToken, authorize("admin"), addDepartment);
-app.get("/", authenticateToken, authorize("admin"), getAllDepartment);
-app.get("/:id", authenticateToken, authorize("admin"), getSingleDepartment);
+app.get("/", authenticateToken, getAllDepartment);
+app.get("/:id", authenticateToken, getSingleDepartment);
 app.put("/:id", authenticateToken, authorize("admin"), updateDepartment);
 app.delete("/:id", authenticateToken, authorize("admin"), deleteDepartment);
 module.exports = router;
