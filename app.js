d5a56bf5e3c3785916e255b06cb2f72c4af18b65
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-28 19:24:58 +0300

    fix: update db connection options

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
+    origin: ["http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
